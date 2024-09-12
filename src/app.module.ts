a594f806ab21410627eea4b81669ebececa418ad
iheboueslati909 <hooba500@gmail.com>
2024-09-12 15:38:51 +0100

    mongoose and env configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,15 @@ import { CommonModule } from './common/common.module';
 import { EventModule } from './event/event.module';
 import { OrganizerModule } from './organizer/organizer.module';
 import { ClubModule } from './club/club.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ArtistModule, CommonModule, EventModule, OrganizerModule, ClubModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017/events'),
+    ArtistModule, CommonModule, EventModule, OrganizerModule, ClubModule,
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true})],
   controllers: [AppController],
   providers: [AppService],
 })
