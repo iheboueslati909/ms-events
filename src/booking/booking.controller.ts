084e5f88b447157d52448cec4d324394a6f98c66
iheboueslati909 <hooba500@gmail.com>
2024-10-30 11:41:54 +0100

    booking controller

--- src/booking/booking.controller.ts
+++ src/booking/booking.controller.ts
@@ -3,7 +3,7 @@ import { GrpcMethod, MessagePattern, Payload } from '@nestjs/microservices';
 import { BookingService } from './booking.service';
 import { CreateBookingDto } from './dto/create-booking.dto';
 import { UpdateBookingDto } from './dto/update-booking.dto';
-import { CreateBookingRequest } from 'src/proto/events-app';
+import { BookingResponse, CreateBookingRequest, DeleteBookingRequest, GetBookingByIdRequest, UpdateBookingRequest } from 'src/proto/events-app';
 
 @Controller()
 export class BookingController {
@@ -13,4 +13,25 @@ export class BookingController {
   createBooking(data: CreateBookingRequest) {
     return this.bookingService.createBooking(data);
   } 
+
+  @GrpcMethod('BookingService', 'GetAllBookings')
+  async getAllBookings() {
+    const Bookings = await this.bookingService.findAllBookings();
+    return {Bookings}
+  }
+
+  @GrpcMethod('BookingService', 'GetBookingById')
+  async getBookingById(data: GetBookingByIdRequest): Promise<BookingResponse> {
+    return this.bookingService.findOneBooking(data.id);
+  }
+
+  @GrpcMethod('BookingService', 'UpdateBooking')
+  async updateBooking(data: UpdateBookingRequest) {
+    return this.bookingService.updateBooking(data.id, data);
+  }
+
+  @GrpcMethod('BookingService', 'DeleteBooking')
+  async deleteBooking(data: DeleteBookingRequest) {
+    return this.bookingService.removeBooking(data.id);
+  }
 }
--- src/booking/booking.module.ts
+++ src/booking/booking.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { BookingService } from './booking.service';
 import { BookingController } from './booking.controller';
+import { CommonModule } from 'src/common/common.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Booking, BookingSchema } from './entities/booking.entity';
 
 @Module({
+  imports: [CommonModule,
+    MongooseModule.forFeature([{
+      name: Booking.name, schema:
+      BookingSchema
+    }])
+  ],
   controllers: [BookingController],
   providers: [BookingService],
+  exports: [BookingService]
 })
 export class BookingModule {}
